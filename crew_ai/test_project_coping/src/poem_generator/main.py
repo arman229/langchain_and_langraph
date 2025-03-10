21308d7f7e0bd46029fb5fefab3f610c113fd651
arman229 <armanashraf015@gmail.com>
2025-03-10 18:08:13 +0500

    working on job matching agent

--- crew_ai/test_project_coping/src/poem_generator/main.py
+++ crew_ai/test_project_coping/src/poem_generator/main.py
@@ -4,25 +4,49 @@ from crewai.flow.flow import listen, start, Flow
 from poem_generator.crews.poem_generator import Personal_crew
 from poem_generator.crews.tool_agent import crew
 
-
-class FinalAns(Flow):
+from poem_generator.crews.read_cv_crew.read_cv import ReadCvCrew
+class MatchProfilePositionFlow(Flow):
 
     @start()
-    def generate_sentence_count(self):
+    def cv_reader_fun(self):
+        inputs = {"cv_path": "./src/poem_generator/crews/read_cv_crew/data/cv.md"}
+        print('hi')
+        crew = ReadCvCrew().crew().kickoff(inputs=inputs)
+        print('crew final result is:',crew.raw)
 
-        resp = crew.kickoff(
-            inputs={
-                "query": "hi i am arman"
-            }
-        )
-        print(f"final_resp: {resp.raw}")
+ 
 
 
 def kickoff():
-    final_poem_flow = FinalAns()
-    final_poem_flow.kickoff()
+    resp = MatchProfilePositionFlow()
+    resp.kickoff()
 
 
 def plot():
-    final_poem_flow = FinalAns()
-    final_poem_flow.plot()
+    resp = MatchProfilePositionFlow()
+    resp.plot()
+
+
+
+
+# class FinalAns(Flow):
+
+#     @start()
+#     def generate_sentence_count(self):
+
+#         resp = crew.kickoff(
+#             inputs={
+#                 "query": "hi i am arman"
+#             }
+#         )
+#         print(f"final_resp: {resp.raw}")
+
+
+# def kickoff():
+#     final_poem_flow = FinalAns()
+#     final_poem_flow.kickoff()
+
+
+# def plot():
+#     final_poem_flow = FinalAns()
+#     final_poem_flow.plot()
