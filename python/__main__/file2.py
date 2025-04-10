8e922e25ac2fea964da7b79f019e382be3f5f670
arman229 <armanashraf015@gmail.com>
2025-04-10 17:24:59 +0500

    learn some new concept

---
+++ python/__main__/file2.py
@@ -0,0 +1,44 @@
+# def file2():
+#     print("This is file2.py") 
+
+# if __name__ == "__main__":
+#     file2()
+    ###########################################################
+    
+# print(__name__)
+# def file2():
+#     print("This is file2.py") 
+ 
+# file2()    
+
+
+    ###########################################################
+# print("This is a script")
+
+# def main():
+#     print("This is the main function of the script.")
+
+# # Running the main function directly
+# if __name__ == "__main__":
+#     main()
+ ############################ 
+def add(a,b):
+    print("This is file2.py")
+    print("This is the add function")
+    return a + b
+def subtract(a,b):
+    print("This is file2.py")
+    print("This is the add function")
+    return a - b
+
+
+# if add(2,3) == 5:
+#     print("Addition is correct")
+# if subtract(5,2) == 3:
+#     print("Subtraction is correct")
+    
+if __name__ == "__main__":
+    print("This is file2.py")
+    print(add(2,4))
+    print(subtract(5,2))
+        
